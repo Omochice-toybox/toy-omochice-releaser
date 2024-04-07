44177f160bfeaadad907ecb7b090c3cd7794985e
Omochice <mochice.mls.ntl@gmail.com>
2024-04-08 03:56:32 +0900

    wip

--- git.ts
+++ git.ts
@@ -1,12 +1,24 @@
 import $ from "https://deno.land/x/dax@0.39.2/mod.ts";
 import * as mod from "https://deno.land/std@0.221.0/semver/mod.ts";
+import { type Commit, sync } from "npm:conventional-commits-parser@5.0.0";
+import createPreset from "npm:conventional-changelog-conventionalcommits@7.0.2";
 
 export async function getTags(branch: string) {
   return (await $`git tag --merged ${branch}`.text())
     .split("\n")
     .map((tag) => tag.trim())
-    .map((tag) => mod.tryParse(tag))
-    .filter(mod.isSemVer);
+    .filter((tag) => tag !== "");
+}
+
+export async function getLatestTag(branch: string) {
+  return (await getTags(branch))
+    .filter(mod.canParse)
+    .sort((a, b) => mod.compare(mod.parse(a)!, mod.parse(b)!))
+    .at(-1);
+}
+
+export async function getTagHead(tagName: string) {
+  return (await $`git rev-list -1 ${tagName}`.text());
 }
 
 export async function getCurrentBranch() {
@@ -34,4 +46,25 @@ export async function sample() {
   Deno.writeTextFileSync("sample.txt", "Hello, World!");
   await $`git add sample.txt`;
   await $`git commit -m "Add sample.txt"`;
+}
+
+export async function getLogs(
+  from: string,
+  to: string = "",
+): Promise<(Commit & { hash: string })[]> {
+  const { parserOpts } = await createPreset();
+  return (await $`git log --reverse --oneline ${from}..${to}`.text())
+    .split("\n")
+    .map((log) => log.trim())
+    .filter((log) => log !== "")
+    .map((log) => {
+      const [hash, ...m] = log.split(" ");
+      const message = m.join(" ");
+      const commit = sync(message, parserOpts);
+      return {
+        ...commit,
+        isBreaking: parserOpts.breakingHeaderPattern?.test(message) ?? false,
+        hash,
+      };
+    });
 }
--- mod.ts
+++ mod.ts
@@ -1,12 +1,16 @@
 import {
   config,
   getCurrentBranch,
+  getLatestTag,
+  getLogs,
+  getTagHead,
   push,
   rebase,
   sample,
   switchBranch,
 } from "./git.ts";
 
+import { parse } from "https://deno.land/std@0.221.0/semver/mod.ts";
 const branchPrefix = "Omochice-releaser-";
 
 if (import.meta.main) {
@@ -15,12 +19,34 @@ if (import.meta.main) {
     console.log("This action is only for main branch.");
     Deno.exit(0);
   }
-  await config({
-    user: "github-action",
-    email: "github-action[bot]@users.noreply.github.com",
-  });
-  await switchBranch(`${branchPrefix}-sample`);
-  await rebase("main");
-  await sample();
-  await push(true);
+  // await config({
+  //   user: "github-action",
+  //   email: "github-action[bot]@users.noreply.github.com",
+  // });
+
+  const tag = await getLatestTag("main");
+  console.log("current tag: ", tag);
+
+  if (tag === undefined) {
+    console.log("No tags found.");
+    Deno.exit(0);
+  }
+
+  const ref = await getTagHead(tag);
+  console.log("head: ", ref);
+
+  const logs = await getLogs(ref);
+  console.log("logs: ", logs);
+}
+
+function bumpup(tag: string, logs) {
+  const { major, minor, patch } = parse(tag)!;
+  const startsWithV = tag.startsWith("v");
+  if (logs.some((l) => l.isBreaking)) {
+    return `${startsWithV ? "v" : ""}${major + 1}.${minor}.${patch}`;
+  }
+  if (logs.some((l) => l.)) {
+}
+
+function getNextVersion(tag: string) {
 }
